663d341d671b3824a432a017893954775a8e860f
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2024-09-05 21:51:29 -0400

    added strings to the arraylist test

--- test/arraylist.c
+++ test/arraylist.c
@@ -1,33 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/nucl.h"
 
 int main() {
     Arraylist arrl = NewArraylist(1);
 
-    printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+    // int test
+    printf("int test:\n");
+    {
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
 
-    int a = 20;
+        int a = 20;
 
-    printf("a: %d\n", a);
+        printf("a: %d\n", a);
 
-    ALAdd(&arrl, (void*)&a);
+        ALAdd(&arrl, (void*)&a);
 
-    printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
 
-    int b = 22;
+        int b = 22;
 
-    printf("b: %d\n", b);
+        printf("b: %d\n", b);
 
-    ALAdd(&arrl, (void*)&b);
+        ALAdd(&arrl, (void*)&b);
 
-    printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
 
-    int b_2 = *(int*)ALRemove(&arrl);
+        int b_2 = *(int*)ALRemove(&arrl);
 
-    printf("a + b: %d\n", *(int*)ALRemove(&arrl) + b_2);
+        printf("a + b: %d\n", *(int*)ALRemove(&arrl) + b_2);
 
-    printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+    }
+
+    // string test
+    printf("\nstring test:\n");
+    {
+        char* a = malloc(sizeof(char) * 5);
+        a = "hello";
+
+        printf("a: '%s'\n", a);
+
+        ALAdd(&arrl, (void*)&a);
+
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+
+        char* b = malloc(sizeof(char) * 5);
+        b = "there";
+
+        printf("b: '%s'\n", b);
+
+        ALAdd(&arrl, (void*)&b);
+
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+
+        char* b_2 = *(char**)ALRemove(&arrl);
+
+        printf("a, b: '%s', '%s'\n", *(char**)ALRemove(&arrl), b_2);
+
+        printf("cap: %d, length: %d\n", ALCap(&arrl), ALLen(&arrl));
+
+        FreeArraylist(&arrl);
+    }
 
     return 0;
 }
